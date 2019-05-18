76a1c97067f5931bf4cd753abf692b0b276190ef
fortuitous-counter-8979 <gmcharmy@gmail.com>
2019-05-18 01:49:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,10 +12,13 @@ function logWhisper(string){
 }
 function sayHiToGrandma(string){
   if(string.toLowerCase===string){
-    
+    return "I can't hear you!";
+  }
+  else if(string.toUpperCase===string){
+   return 
   }
 }
-}
+
 
     
     
